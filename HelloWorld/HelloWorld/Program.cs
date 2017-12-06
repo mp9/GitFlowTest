70f10d4462f9a0ee3a5e0b17729f35dfd239c0e6
kevin.hsu <kevinhsu@kevinhsudeMacBook-Pro.local>
2017-12-06 10:09:27 +0800

    master v1.0

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            DownLoadWeb();
+            Console.ReadKey();
+        }
+
+        private static void DownLoadWeb()
+        {
+            var client = new HttpClient();
+
+        }
+    }
+}
